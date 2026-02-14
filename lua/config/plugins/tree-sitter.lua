5893aafadfb54ae236572c5f9c9bb514d9afd8b0
kakeetopius <pius.kakeeto@strathmore.edu>
2026-02-14 22:47:40 +0300

    Updates

--- lua/config/plugins/tree-sitter.lua
+++ lua/config/plugins/tree-sitter.lua
@@ -2,51 +2,67 @@
 -- making highlighting, indentation, and code navigation more accurate and language-aware.
 
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        lazy = false,
-        build = ":TSUpdate",
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "lua",
-                    "python",
-                    "javascript",
-                    "html",
-                    "json",
-                    "c",
-                    "cpp",
-                    "bash",
-                    "markdown",
-                    "markdown_inline",
-                    "java",
-                    "vim",
-                    "query",
-                    "css",
-                    "typescript",
-                    "tsx",
-                    "yaml",
-                    "toml",
-                    "rust",
-                    "go",
-                    "php",
-                    "latex",
-                    "gotmpl",
-                },
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    build = ":TSUpdate",
+    branch = "main",
+    config = function()
+        local ts = require("nvim-treesitter")
+        local parsers = {
+            "bash",
+            "comment",
+            "css",
+            "diff",
+            "dockerfile",
+            "git_config",
+            "gitcommit",
+            "gitignore",
+            "go",
+            "html",
+            "http",
+            "java",
+            "javascript",
+            "jsdoc",
+            "json",
+            "json5",
+            "lua",
+            "make",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "regex",
+            "rst",
+            "rust",
+            "ssh_config",
+            "sql",
+            "terraform",
+            "toml",
+            "tsx",
+            "typescript",
+            "vim",
+            "vimdoc",
+            "yaml",
+        }
 
-                sync_install = false,
-                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-                auto_install = false,
+        for _, parser in ipairs(parsers) do
+            ts.install(parser)
+        end
 
-                indent = { enable = true },
-                autotag = true,
+        -- Not every tree-sitter parser is the same as the file type detected
+        -- So the patterns need to be registered more cleverly
+        local patterns = {}
+        for _, parser in ipairs(parsers) do
+            local parser_patterns = vim.treesitter.language.get_filetypes(parser)
+            for _, pp in pairs(parser_patterns) do
+                table.insert(patterns, pp)
+            end
+        end
 
-                highlight = {
-                    enable = true,
-                    disable = { "c", "rust" },
-                    additional_vim_regex_highlighting = true,
-                },
-            })
-        end,
-    },
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = patterns,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
 }
