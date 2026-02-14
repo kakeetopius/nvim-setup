d5bccfad03ee04256bf416a59dd1bc20a0aa2657
kakeetopius <pius.kakeeto@strathmore.edu>
2026-02-14 21:39:07 +0300

    Updates

--- lua/config/plugins/tree-sitter.lua
+++ lua/config/plugins/tree-sitter.lua
@@ -5,7 +5,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
-        build = "TSUpdate",
+        build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = {
